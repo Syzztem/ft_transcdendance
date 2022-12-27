00d95fffde738b1cc5f29ea812c839463bc993f5
Antoine EDOUARD <antoine.edouard@gmail.com>
2022-12-27 21:24:41 +0100

    basic css addition

--- front/app/src/components/Chat.vue
+++ front/app/src/components/Chat.vue
@@ -10,7 +10,7 @@ export default {
     {
         sendmsg() 
         {
-            let send = <HTMLElement> document.getElementById('id_newmsg')
+            let send = <HTMLElement> document.getElementById('chatbox')
             let input = <any> document.getElementById('inputid')
             send.innerHTML += input.value + '<br/>'
             input.value = ''
@@ -22,15 +22,48 @@ export default {
 
 <template>
     <v-container class = "container">
-        <div id="id_newmsg"></div>
-        <input @change='sendmsg' id='inputid'>
+        <div class="row">
+            <div id="chatbox"  class="column"></div>
+        <div id="userlist" class="column">users</div>
+        <div id="chanlist" class="column">chans</div>
+        </div>
     </v-container>
+    <input @change='sendmsg' id='inputid'>
+    
   </template>
 
 <style scoped>
 .container
 {
-    color:red;
+    background-color: rgb(145, 255, 0);
+    display: flex;
+    content: "";
+    display: table;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+#chatbox
+{
+    background-color: rgb(17, 17, 194);
+    color: rgb(53, 187, 0);
+}
+#userlist
+{
+    background-color: red;
+}
+#chanlist
+{
+    background-color: green;
+}
+
+#column {
+  float: left;
+  width: 33.33%;
 }
 </style>
 
